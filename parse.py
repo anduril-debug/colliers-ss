8042b0685dd8a345b75c2fa5e93cce7e755f906a
parseSS <anduril-debug>
2021-05-05 13:30:09 +0400

    parsing with functions one by one

--- parse.py
+++ parse.py
@@ -4,7 +4,23 @@ from spider_items import FlatSpider
 
 
 
-process = CrawlerProcess()
-process.crawl(FlatLinksSpider)
-process.crawl(FlatSpider)
-process.start()
+
+def parse_flat_links():
+    print("STARTING SCRAPING FLAT LINKS....")
+
+    process = CrawlerProcess()
+    process.crawl(FlatLinksSpider)
+    process.start()
+
+
+def parse_flats():
+    print("STARTING SCRAPING FLATS....")
+    
+    process = CrawlerProcess()
+    process.crawl(FlatSpider)
+    process.start()
+
+
+
+parse_flat_links()
+parse_flats()
