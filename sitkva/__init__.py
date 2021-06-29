af8940c8da77cb56ce05f4c05b9c484bfbe3bae9
anduril-debug <lukalortk16@gmail.com>
2021-06-29 13:16:39 +0400

    security changes

--- sitkva/__init__.py
+++ sitkva/__init__.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -10,7 +12,18 @@ app = Flask(__name__)
 
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345678@localhost:5432/SitkvaSakmeDB"
+load_dotenv()
+
+DB_USER = os.environ.get("LOCAL_DB_USERNAME")
+DB_USER_PASS = os.environ.get("LOCAL_DB_PASSWORD")
+LOCAL_SERVER_HOST = os.environ.get("LOCAL_SERVER_HOST")
+LOCAL_SERVER_PORT = os.environ.get("LOCAL_SERVER_PORT")
+LOCAL_SERVER_DB = os.environ.get("LOCAL_SERVER_DB")
+
+
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{DB_USER}:{DB_USER_PASS}@{LOCAL_SERVER_HOST}:{LOCAL_SERVER_PORT}/{LOCAL_SERVER_DB}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- to_sql.py
+++ to_sql.py
@@ -1,12 +1,14 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+SERVER = os.environ.get("COLLIERS_SQL_ADDRESS")
+USERNAME = os.environ.get("COLLIERS_USER")
+PASSWORD = os.environ.get("COLLIERS_USER_PASSWORD")
+DATABASE = os.environ.get("COLLIERS_DATABASE")
 
-server = "192.168.0.36"
-username = "Valuation_User"
-password = "q2b8dy8VH^W!kkU"
-database = "Valuation"
-
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+SERVER+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD)
 cursor = cnxn.cursor()
 
 
